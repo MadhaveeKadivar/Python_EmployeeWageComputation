8bc2f168d229cfb43ea98b4d2f096d99353a304b
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-12 13:11:27 +0530

    [Madhavee] Add : Code to check employee is present or absent

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,1 +1,17 @@
-print("Welcome to the Employee Wage Computation")
+
+'''
+    @Author: Madhavee Kadivar
+    @Date: 2022-05-12 12:55:01
+    @Last Modified by: Madhavee Kadivar
+    @Last Modified time: 2022-05-12 12:55:01
+    @Title : Employee Wage Computation
+'''
+import random
+# Checking that employee is present or not using random module
+check = random.randint(0,1)
+if(check == 1):
+    print("Employee is Present")
+else:
+    print("Employee is Absent")
+
+
