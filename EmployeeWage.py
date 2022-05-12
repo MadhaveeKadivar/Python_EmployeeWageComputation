a8ef0ae9f6081c9e070fbc31c58d4c5ef4636001
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-12 18:12:42 +0530

    [Madhavee] Add : Switch Case for Employee Attendence

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,47 +3,70 @@
     @Author: Madhavee Kadivar
     @Date: 2022-05-12 12:55:01
     @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-12 4:40:26
+    @Last Modified time: 2022-05-12 5:45:30
     @Title : Employee Wage Computation
 '''
 # Importing random modules
+from email.policy import default
 import random
+
 # Declaring variables
 wagePerHour = 20
-empWorkHour = empDailyWage = totalMonthWage = empTotalHour = empTotalWorkDays = 0
+empWorkHour = 0
+empDailyWage = 0
+
 # Checking that employee is present for full time , part time or absent
-def GetWorkHours(check):
+def PresentForFullTime():
     """ 
         Description: 
-            This function is calculating workhours based on employee attendence
+            This function is set employee work hours as 8 for full time presence of employee
         Parameter:
-            It takes one integer as argument
+            None
         Return:
-            returns employee work hours for one day 
+            Nothing
     """
-    if(check == 1):
-        empWorkHour = 8 # Employee is present for full time
-    elif(check == 2):
-        empWorkHour = 4  # Employee is present for part time
-    else:
-        empWorkHour = 0  # Employee is absent
+    empWorkHour = 8
     return empWorkHour
+def PresentForPartTime():
+    """ 
+        Description: 
+            This function is set employee work hours as 4 for part time presence of employee
+        Parameter:
+            None
+        Return:
+            Nothing
+    """
+    empWorkHour = 4
+    return empWorkHour
+def Absent():
+    """ 
+        Description: 
+            This function is set employee work hours as 0 for absence of employee
+        Parameter:
+            None
+        Return:
+            Nothing
+    """
+    empWorkHour = 0
+    return empWorkHour
+def SwitchCase(check):
+    """ 
+        Description: 
+            This function is used for imlementing switch case for employee attendence
+        Parameter:
+            This function takes one integer parameter 
+        Return:
+            It returns function value based on choice
+    """
+    switch={
+       1 : PresentForFullTime(),
+       2 : PresentForPartTime(),
+       0 : Absent(),
+    }
+    return switch.get(check,"")
+check = random.randint(0,2)
+result = SwitchCase(check) # Calling function for cases
 
-while(empTotalHour<=100 and empTotalWorkDays<20):
-    check = random.randint(0,2)
-    empWorkHour = GetWorkHours(check) # Calling function to get work hours
-    empDailyWage = empWorkHour * wagePerHour; # Calculating employee daily wage based on work hours
-    totalMonthWage += empDailyWage # Adding daily wage to total wages
-    empTotalHour += empWorkHour
-    print(f"Employee daily wage for day {empTotalWorkDays+1} is : {empDailyWage}") # Storing daily wage with day  
-    empTotalWorkDays += 1
-    
-if (empTotalHour > 100): # Checking that hours are more than 100 or not
-    a = empTotalHour - 100
-    empTotalHour -= a
-    wage = a * wagePerHour # Calculate extra hours wage
-    totalMonthWage -= wage; # Minus extra hours wage from emp total wage
-    
-print(f"\n\nEmployee total working days  : {empTotalWorkDays}")
-print(f"Employee total working hours  : {empTotalHour}")
-print(f"Employee Total Month Wage : {totalMonthWage}")
+# Calculating employee daily wage based on work hours
+empDailyWage = result*wagePerHour
+print(f"Employee Daily wage is : {empDailyWage}")
