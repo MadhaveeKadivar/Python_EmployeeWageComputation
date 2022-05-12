bd098cd3d169650ce84de09672507609df8d59b6
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-12 16:45:14 +0530

    [Madhavee] Add : Function to get work hours of employee to calculate month wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,7 +3,7 @@
     @Author: Madhavee Kadivar
     @Date: 2022-05-12 12:55:01
     @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-12 4:20:10
+    @Last Modified time: 2022-05-12 4:40:26
     @Title : Employee Wage Computation
 '''
 # Importing random modules
@@ -12,17 +12,26 @@ import random
 wagePerHour = 20
 empWorkHour = empDailyWage = totalMonthWage = empTotalHour = empTotalWorkDays = 0
 # Checking that employee is present for full time , part time or absent
+def GetWorkHours(check):
+    """ 
+        Description: 
+            This function is calculating workhours based on employee attendence
+        Parameter:
+            It takes one integer as argument
+        Return:
+            returns employee work hours for one day 
+    """
+    if(check == 1):
+        empWorkHour = 8 # Employee is present for full time
+    elif(check == 2):
+        empWorkHour = 4  # Employee is present for part time
+    else:
+        empWorkHour = 0  # Employee is absent
+    return empWorkHour
+
 while(empTotalHour<=100 and empTotalWorkDays<20):
     check = random.randint(0,2)
-    if(check == 1):
-        print("Employee is Present for Full time")
-        empWorkHour = 8
-    elif(check == 2):
-        print("Employee is Present for Part time")
-        empWorkHour = 4
-    else:
-        print("Employee is Absent")
-        empWorkHour = 0
+    empWorkHour = GetWorkHours(check) # Calling function to get work hours
     empDailyWage = empWorkHour * wagePerHour; # Calculating employee daily wage based on work hours
     totalMonthWage += empDailyWage # Adding daily wage to total wages
     empTotalWorkDays += 1
