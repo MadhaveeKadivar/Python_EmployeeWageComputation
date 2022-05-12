ba4fe3b68c489394de161b2a0e9c0964e1c0fa78
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-12 13:51:28 +0530

    [Madhavee] Add : Code for calculating daily wage for employee with part time presence

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,7 +3,7 @@
     @Author: Madhavee Kadivar
     @Date: 2022-05-12 12:55:01
     @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-12 1:30:30
+    @Last Modified time: 2022-05-12 1:49:00
     @Title : Employee Wage Computation
 '''
 # Importing random modules
@@ -12,14 +12,17 @@ import random
 wagePerHour = 20
 empWorkHour = 0
 empDailyWage = 0
-# Checking that employee is present or absent
-check = random.randint(0,1)
+# Checking that employee is present for full time , part time or absent
+check = random.randint(0,2)
 if(check == 1):
-    print("Employee is Present")
+    print("Employee is Present for Full time")
     empWorkHour = 8
+elif(check == 2):
+    print("Employee is Present for Part time")
+    empWorkHour = 4
 else:
     print("Employee is Absent")
     empWorkHour = 0
-# Calculating employee daily wage  
+# Calculating employee daily wage based on work hours
 empDailyWage = empWorkHour*wagePerHour
 print(f"Employee Daily wage is : {empDailyWage}")
