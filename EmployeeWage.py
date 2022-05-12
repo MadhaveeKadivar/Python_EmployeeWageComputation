11f23cf7eb7c217737399f429d3118f610f113a3
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-05-12 13:36:17 +0530

    [Madhavee] Add : Code for calculating daily wage for employee

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -3,15 +3,23 @@
     @Author: Madhavee Kadivar
     @Date: 2022-05-12 12:55:01
     @Last Modified by: Madhavee Kadivar
-    @Last Modified time: 2022-05-12 12:55:01
+    @Last Modified time: 2022-05-12 1:30:30
     @Title : Employee Wage Computation
 '''
+# Importing random modules
 import random
-# Checking that employee is present or not using random module
+# Declaring variables
+wagePerHour = 20
+empWorkHour = 0
+empDailyWage = 0
+# Checking that employee is present or absent
 check = random.randint(0,1)
 if(check == 1):
     print("Employee is Present")
+    empWorkHour = 8
 else:
     print("Employee is Absent")
-
-
+    empWorkHour = 0
+# Calculating employee daily wage  
+empDailyWage = empWorkHour*wagePerHour
+print(f"Employee Daily wage is : {empDailyWage}")
